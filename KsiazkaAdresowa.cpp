15d3b4e9e7cc662b218ca7032f96073ebef9221c
Anna <anna.gromczak@gmail.com>
2018-12-15 11:05:12 +0100

    Created feature: user login, password change and user logout

--- KsiazkaAdresowa.cpp
+++ KsiazkaAdresowa.cpp
@@ -5,6 +5,21 @@ void KsiazkaAdresowa::rejestracjaUzytkownika()
     uzytkownikManager.rejestracjaUzytkownika();
 }
 
+int KsiazkaAdresowa::logowanieUzytkownika()
+{
+    uzytkownikManager.logowanieUzytkownika();
+}
+
+void KsiazkaAdresowa::zmianaHaslaZalogowanegoUzytkownika()
+{
+    uzytkownikManager.zmianaHaslaZalogowanegoUzytkownika();
+}
+
+void KsiazkaAdresowa::wylogujUzytkownika()
+{
+    uzytkownikManager.wylogujUzytkownika();
+}
+
 void KsiazkaAdresowa::wypiszWszystkichUzytkownikow()
 {
     uzytkownikManager.wypiszWszystkichUzytkownikow();
